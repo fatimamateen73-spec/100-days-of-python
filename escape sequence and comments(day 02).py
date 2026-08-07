6e618f892252ba7287de3557a0166bf03bdb000d
Fatima Mateen <fatimamateen73@gmail.com>
2026-08-07 13:35:51 +0500

    escape sequences and comments

---
+++ escape sequence and comments(day 02).py
@@ -0,0 +1,13 @@
+print("i have an orange cat \n and he is very cute")
+# never remove a code about cats
+print("i have a black cat \t and he is very cute")
+# this is a comment about the black cat
+print("i have a \"white cat\" and he is very cute")
+# this is a comment about the white cat
+'''this is a multiline comment
+we will write multiple lines
+and enclose it using tripple quotes'''
+print("i have a brown cat \r and he is very cute")
+print("hello",3,4)
+print("hello",3,4,sep="*")
+print("hello",3,4,sep="*",end="t")
